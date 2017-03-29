d978245e2b1d28133f44c8a91fb559c145fdc186
Matthew Elger <mattelger@icloud.com>
2017-03-29 12:03:10 +0100

    Add files via upload

---
+++ Holidays/Holidays/Customers.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCustomers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtPostcode = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.lblPostCode = New System.Windows.Forms.Label()
+        Me.txtCustomerID = New System.Windows.Forms.TextBox()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.lblAddress = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblCustomerID = New System.Windows.Forms.Label()
+        Me.btnRetrieve = New System.Windows.Forms.Button()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.dtpDOB = New System.Windows.Forms.DateTimePicker()
+        Me.cmbCustomerID = New System.Windows.Forms.ComboBox()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnSave.Location = New System.Drawing.Point(4, 226)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(54, 32)
+        Me.btnSave.TabIndex = 31
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(35, 3)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(129, 29)
+        Me.lblName.TabIndex = 25
+        Me.lblName.Text = "Customers"
+        '
+        'txtPostcode
+        '
+        Me.txtPostcode.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtPostcode.Location = New System.Drawing.Point(138, 147)
+        Me.txtPostcode.Name = "txtPostcode"
+        Me.txtPostcode.Size = New System.Drawing.Size(134, 20)
+        Me.txtPostcode.TabIndex = 24
+        '
+        'txtAddress
+        '
+        Me.txtAddress.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtAddress.Location = New System.Drawing.Point(138, 121)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(134, 20)
+        Me.txtAddress.TabIndex = 22
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtFirstName.Location = New System.Drawing.Point(138, 69)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(134, 20)
+        Me.txtFirstName.TabIndex = 21
+        '
+        'lblPostCode
+        '
+        Me.lblPostCode.AutoSize = True
+        Me.lblPostCode.Location = New System.Drawing.Point(48, 147)
+        Me.lblPostCode.Name = "lblPostCode"
+        Me.lblPostCode.Size = New System.Drawing.Size(52, 13)
+        Me.lblPostCode.TabIndex = 30
+        Me.lblPostCode.Text = "Postcode"
+        '
+        'txtCustomerID
+        '
+        Me.txtCustomerID.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtCustomerID.Location = New System.Drawing.Point(138, 45)
+        Me.txtCustomerID.Name = "txtCustomerID"
+        Me.txtCustomerID.Size = New System.Drawing.Size(134, 20)
+        Me.txtCustomerID.TabIndex = 20
+        '
+        'lblDOB
+        '
+        Me.lblDOB.AutoSize = True
+        Me.lblDOB.Location = New System.Drawing.Point(48, 179)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(66, 13)
+        Me.lblDOB.TabIndex = 29
+        Me.lblDOB.Text = "Date of Birth"
+        '
+        'lblAddress
+        '
+        Me.lblAddress.AutoSize = True
+        Me.lblAddress.Location = New System.Drawing.Point(48, 121)
+        Me.lblAddress.Name = "lblAddress"
+        Me.lblAddress.Size = New System.Drawing.Size(45, 13)
+        Me.lblAddress.TabIndex = 28
+        Me.lblAddress.Text = "Address"
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(48, 69)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(57, 13)
+        Me.lblFirstName.TabIndex = 27
+        Me.lblFirstName.Text = "First Name"
+        '
+        'lblCustomerID
+        '
+        Me.lblCustomerID.AutoSize = True
+        Me.lblCustomerID.Location = New System.Drawing.Point(48, 45)
+        Me.lblCustomerID.Name = "lblCustomerID"
+        Me.lblCustomerID.Size = New System.Drawing.Size(62, 13)
+        Me.lblCustomerID.TabIndex = 26
+        Me.lblCustomerID.Text = "CustomerID"
+        '
+        'btnRetrieve
+        '
+        Me.btnRetrieve.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnRetrieve.Location = New System.Drawing.Point(203, 226)
+        Me.btnRetrieve.Name = "btnRetrieve"
+        Me.btnRetrieve.Size = New System.Drawing.Size(69, 32)
+        Me.btnRetrieve.TabIndex = 19
+        Me.btnRetrieve.Text = "Retrieve"
+        Me.btnRetrieve.UseVisualStyleBackColor = False
+        '
+        'txtLastName
+        '
+        Me.txtLastName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtLastName.Location = New System.Drawing.Point(138, 95)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(134, 20)
+        Me.txtLastName.TabIndex = 32
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(48, 95)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(58, 13)
+        Me.lblLastName.TabIndex = 33
+        Me.lblLastName.Text = "Last Name"
+        '
+        'dtpDOB
+        '
+        Me.dtpDOB.Location = New System.Drawing.Point(138, 173)
+        Me.dtpDOB.Name = "dtpDOB"
+        Me.dtpDOB.Size = New System.Drawing.Size(134, 20)
+        Me.dtpDOB.TabIndex = 34
+        '
+        'cmbCustomerID
+        '
+        Me.cmbCustomerID.FormattingEnabled = True
+        Me.cmbCustomerID.Location = New System.Drawing.Point(64, 226)
+        Me.cmbCustomerID.Name = "cmbCustomerID"
+        Me.cmbCustomerID.Size = New System.Drawing.Size(133, 21)
+        Me.cmbCustomerID.TabIndex = 35
+        Me.cmbCustomerID.Visible = False
+        '
+        'btnBack
+        '
+        Me.btnBack.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnBack.Location = New System.Drawing.Point(203, 226)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(69, 32)
+        Me.btnBack.TabIndex = 36
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = False
+        Me.btnBack.Visible = False
+        '
+        'frmCustomers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.cmbCustomerID)
+        Me.Controls.Add(Me.dtpDOB)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.txtPostcode)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.lblPostCode)
+        Me.Controls.Add(Me.txtCustomerID)
+        Me.Controls.Add(Me.lblDOB)
+        Me.Controls.Add(Me.lblAddress)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.lblCustomerID)
+        Me.Controls.Add(Me.btnRetrieve)
+        Me.Name = "frmCustomers"
+        Me.Text = "Customers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtPostcode As System.Windows.Forms.TextBox
+    Friend WithEvents txtAddress As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents lblPostCode As System.Windows.Forms.Label
+    Friend WithEvents txtCustomerID As System.Windows.Forms.TextBox
+    Friend WithEvents lblDOB As System.Windows.Forms.Label
+    Private WithEvents lblAddress As System.Windows.Forms.Label
+    Friend WithEvents lblFirstName As System.Windows.Forms.Label
+    Friend WithEvents lblCustomerID As System.Windows.Forms.Label
+    Friend WithEvents btnRetrieve As System.Windows.Forms.Button
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents lblLastName As System.Windows.Forms.Label
+    Friend WithEvents dtpDOB As System.Windows.Forms.DateTimePicker
+    Friend WithEvents cmbCustomerID As System.Windows.Forms.ComboBox
+    Friend WithEvents btnBack As System.Windows.Forms.Button
+End Class
---
+++ Holidays/Holidays/Customers.vb
@@ -0,0 +1,151 @@
+﻿Imports System.IO
+
+Public Class frmCustomers
+    Private Structure Customers
+        Public CustomerID As String
+        Public FirstName As String
+        Public LastName As String
+        Public Address As String
+        Public Postcode As String                  'Creating the structure that will hold the data.
+        Public DOB As String
+    End Structure
+
+    Private Sub cmdSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+
+        Dim MissingData As Integer 'if you haven't entered any data into the textboxes it will be added to missing data variable to later do a presence check.
+        If txtFirstName.Text = "" Then MissingData = MissingData + 1
+        If txtAddress.Text = "" Then MissingData = MissingData + 1
+        If txtCustomerID.Text = "" Then MissingData = MissingData + 1
+        If txtPostcode.Text = "" Then MissingData = MissingData + 1
+        If txtLastName.Text = "" Then MissingData = MissingData + 1
+        If MissingData >= 1 Then 'presence check on the data input by the user
+            MsgBox("You are missing " & MissingData & " items of data.")
+            Exit Sub
+        End If
+
+        Dim CustomerDataCheck() As String = File.ReadAllLines(Dir$("Customers.txt"))
+        For I = 0 To UBound(CustomerDataCheck)
+            If txtCustomerID.Text = Trim(Mid(CustomerDataCheck(I), 1, 5)) Then 'checks if a record already has the client id entered.
+                MsgBox("There is already a record with that Customer ID.")
+                Exit Sub
+            End If
+        Next I
+
+        If txtCustomerID.Text.Length > 5 Then
+            MsgBox("The ClientID you have entered is too long.")
+            Exit Sub
+        End If
+
+        If txtFirstName.Text.Length > 30 Then
+            MsgBox("The First Name you have entered is too long.")
+            Exit Sub
+        End If
+
+        If txtLastName.Text.Length > 30 Then
+            MsgBox("The Last Name you have entered is too long.")
+            Exit Sub
+        End If
+
+        If txtAddress.Text.Length > 30 Then
+            MsgBox("The Address you have entered is too long.")
+            Exit Sub
+        End If
+        If CStr((txtPostcode.Text.Replace(" ", ""))).Length <> 7 Then
+            MsgBox("The Postcode you have entered is not a valid postcode.")
+            Exit Sub
+        End If
+
+        'checks if the data entered by the user is already a record in the database.
+        For I = 0 To UBound(CustomerDataCheck)
+            Dim CountGot As Integer
+            If Trim(Mid(CustomerDataCheck(I), 6, 30)) = txtFirstName.Text Then CountGot = CountGot + 1 'Checks eack item of data entered by the user.
+            If Trim(Mid(CustomerDataCheck(I), 36, 30)) = txtLastName.Text Then CountGot = CountGot + 1
+            If Trim(Mid(CustomerDataCheck(I), 66, 30)) = txtAddress.Text Then CountGot = CountGot + 1
+            If Trim(Mid(CustomerDataCheck(I), 106, 20)) = dtpDOB.Text Then CountGot = CountGot + 1
+            If Trim(Mid(CustomerDataCheck(I), 96, 10)) = txtPostcode.Text Then CountGot = CountGot + 1
+            If Trim(Mid(CustomerDataCheck(I), 1, 5)) = txtCustomerID.Text Then CountGot = CountGot + 1
+            If CountGot = 7 Then 'If all 8 items of data are the same as an already stored record the user is notified and the record is not saved twice.
+                MsgBox("Record is already stored on out system.")
+                Exit Sub
+            End If
+        Next I
+
+        Dim CustomerData As New Customers
+        Dim sw As New System.IO.StreamWriter("Customers.txt", True)
+        CustomerData.CustomerID = LSet(txtCustomerID.Text, 5)
+        CustomerData.FirstName = LSet(txtFirstName.Text, 30)
+        CustomerData.LastName = LSet(txtLastName.Text, 30)
+        CustomerData.Address = LSet(txtAddress.Text, 30)
+        CustomerData.Postcode = LSet(txtPostcode.Text, 10) 'Filling the structure with data.
+        CustomerData.DOB = LSet(dtpDOB.Text, 20)
+
+        sw.WriteLine(CustomerData.CustomerID & CustomerData.FirstName & CustomerData.LastName & CustomerData.Address & CustomerData.Postcode & CustomerData.DOB)
+        sw.Close()                                                                  'Always need to close afterwards
+        MsgBox("File Saved!")
+    End Sub
+
+    Private Sub btnRetrieve_Click(sender As System.Object, e As System.EventArgs) Handles btnRetrieve.Click
+        btnRetrieve.Visible = False
+        btnBack.Visible = True
+        cmbCustomerID.Visible = True
+        btnSave.Visible = False
+        txtAddress.Enabled = False
+        txtPostcode.Enabled = False
+        txtFirstName.Enabled = False
+        txtLastName.Enabled = False
+        dtpDOB.Enabled = False
+        txtCustomerID.Enabled = False
+        Dim CustomerData() As String = File.ReadAllLines(Dir$("Customers.txt")) 'gets the data inside the text box linked to the program.
+        For I = 0 To UBound(CustomerData) 'retrives all the records client ids stored inside the text document and outputs them in a combo box for the user to select a record from.
+            cmbCustomerID.Items.Add(Trim(Mid(CustomerData(I), 1, 5)))
+        Next I
+    End Sub
+
+    Private Sub frmCustomers_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        If Dir$("Customers.txt") = "" Then
+            Dim sw As New StreamWriter("Customers.txt", True)    'This makes sure there is actually a database to enter/read data. If not, it creates a new blank one.
+            sw.WriteLine("")
+            sw.Close()
+            MsgBox("A new file has been created", vbExclamation, "Warning!")
+        End If
+    End Sub
+
+    Private Sub btnBack_Click(sender As System.Object, e As System.EventArgs) Handles btnBack.Click
+        'resets for a new record to be entered.
+        btnRetrieve.Visible = True
+        btnBack.Visible = False
+        cmbCustomerID.Visible = False
+        btnSave.Visible = True
+        txtAddress.Text = ""
+        txtCustomerID.Text = ""
+        txtFirstName.Text = ""
+        txtLastName.Text = ""
+        txtPostcode.Text = ""
+        dtpDOB.Value = Date.Now
+        txtAddress.Enabled = True
+        txtPostcode.Enabled = True
+        txtFirstName.Enabled = True
+        txtLastName.Enabled = True
+        txtCustomerID.Enabled = True
+        dtpDOB.Enabled = True
+        cmbCustomerID.Items.Clear()
+    End Sub
+
+    Private Sub cmbCustomerID_SelectedIndexChanged(sender As Object, e As System.EventArgs) Handles cmbCustomerID.SelectedIndexChanged
+        Dim CustomerData() As String = File.ReadAllLines(Dir$("Customers.txt")) 'gets the data inside the text box linked to the program.
+
+        For I = 0 To UBound(CustomerData) 'retrives all the records stored inside the text document and outputs them in a message box for the user.
+            If Trim(Mid(CustomerData(I), 1, 5)) = cmbCustomerID.SelectedItem Then
+                txtFirstName.Text = Trim(Mid(CustomerData(I), 6, 30))
+                txtLastName.Text = Trim(Mid(CustomerData(I), 36, 30))
+                txtAddress.Text = Trim(Mid(CustomerData(I), 66, 30))
+                txtPostcode.Text = Trim(Mid(CustomerData(I), 96, 10))
+                If Trim(Mid(CustomerData(I), 106, 20)) = "" Then
+                Else
+                    dtpDOB.Value = Trim(Mid(CustomerData(I), 106, 20)) & " 11:30 PM"
+                End If
+                txtCustomerID.Text = Trim(Mid(CustomerData(I), 1, 5))
+            End If
+        Next I
+    End Sub
+End Class
